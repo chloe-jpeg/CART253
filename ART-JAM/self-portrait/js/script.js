87a6271f5bc9322f3c40375b9d57b9cc1ed5dde4
chloe-jpeg <chloe.guerin@gmail.com>
2025-09-22 22:34:29 -0400

    ears and hair work
    did the ears pretty quickly, and now i'm just messing around with hair, trying different stuff and hoping to find something good. bangs are my next step

--- ART-JAM/self-portrait/js/script.js
+++ ART-JAM/self-portrait/js/script.js
@@ -27,7 +27,9 @@ function draw() {
 
     drawHair()
     drawNeck()
+    drawEars()
     drawFace()
+    drawBangs()
     drawHoodie()
     drawNose()
     drawMouth()
@@ -40,24 +42,52 @@ function drawFace() {
     ellipse(300, 300, 200, 250)
 }
 
-//alright challenge:try to make the hair in less than an hour
+//alright challenge:try to make the hair in less than an hour(i was doing something sooo complicated for nothing)
 function drawHair() {
-    noStroke()
+    rectMode(CENTER)
     fill(0)
-    ellipse(300, 300, 250, 300)
+    rect(300, 315, 235, 305, 80)
+
+
+    //space buns! 
+    fill(0)
+    ellipse(220, 190, 100)
+    fill(0)
+    ellipse(380, 190, 100)
 }
 
-//ok i'm not gonna lie, i'm following a p5 tuto for that one, i'll credit in read me. also is the no stroke necessary?
+//jumping all over the place but hair is annoying so bangs!
+//function drawBangs() {
+// fill(255)
+// arc(width / 2.5, height / 3, 140, 140, .90, 180);
+//}
+
+//second try at bangs, have a genius idea
+
+
+
+//ok i'm not gonna lie, i'm following a p5 tuto for that one, i'll credit in read me. also is the no stroke necessary? (worked on  the hair and now my rectangle is gone??)((function draw bangs got the rect back??))
 function drawHoodie() {
     rectMode(CENTER)
     fill(0)
     rect(300, 550, 300, 200)
 }
 
-//neck i need a win. eeeeh i'll take it. nostroke is not necessary! is the order of things important? i guess for style and understanding but rn i'm feeling chaotic. i'll clean up later. also editing these is gonna be a bitch, but at least it's fun!
+//neck i need a win. eeeeh i'll take it. nostroke is not necessary! is the order of things important? i guess for style and understanding but rn i'm feeling chaotic. i'll clean up later. also editing these is gonna be a bitch, but at least it's fun now!
 function drawNeck() {
     rectMode(CENTER)
+    noStroke()
     fill(209, 171, 134)
     rect(300, 450, 80, 90)
 }
 
+function drawEars() {
+
+    //right ear or so i'm trying
+    fill(209, 171, 134)
+    ellipse(215, 310, 70)
+
+    //ok that worked now left ear!
+    fill(209, 171, 134)
+    ellipse(385, 310, 70)
+}
